d57f29acbf5f67a714f4d2b94d335956437a179d
lilyhoratio <lilyhzhou@gmail.com>
2019-09-20 08:54:28 -0700

    registration post request - hashed password w/ bcrypt and inserted user into db

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,10 +1,30 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const db = require("../database/dbConfig.js");
 
-router.post('/register', (req, res) => {
+router.post("/register", (req, res) => {
   // implement registration
+  let { username, password } = req.body;
+  const hash = bcrypt.hashSync(password, 8);
+
+  db("users")
+    .insert({ username, password: hash }, "id")
+    .then(([id]) => {
+      // find user by id, returns user info
+      return db("users")
+        .where({ id })
+        .first();
+    })
+    .then(user => {
+      res.status(201).json(user);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   // implement login
 });
 
